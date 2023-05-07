055dd7f41d4f485cc73fb8bdd611352bce66e42f
NicolasBauchi <bauchinicolas@gmail.com>
2023-05-07 12:21:05 -0300

    Agregando env

--- src/config/objectConfig.js
+++ src/config/objectConfig.js
@@ -1,8 +1,6 @@
 import { connect } from "mongoose";
 require('dotenv').config()
 let url = process.env.MONGO_URL
-
-//let url = "mongodb+srv://bauchinicolas:czSdWV0eQZ8Tmpxh@clusterneb.oqrfnt8.mongodb.net/ecommerce?retryWrites=true&w=majority"; 
 // aca poner URL de conexion a DB mongo ATLAS ONLINE.
 
 const objectConfig = {
